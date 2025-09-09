167734b56a6a89a3c0c04933c9dbd20b1c50b11e
Annisa Dinda <100127500+annisaadg@users.noreply.github.com>
2025-09-09 07:36:35 +0700

    remove env

--- params.py
+++ params.py
@@ -1,17 +1,7 @@
-import torch
 from ultralytics import YOLO
-import os
 
-# Folder berisi model YOLOv8 (.pt)
-model_folder = 'models'
+# Load your YOLOv8 model
+model = YOLO('models/Baseline.pt')  # Replace with your model path
 
-# Loop semua file .pt di folder
-for filename in os.listdir(model_folder):
-    if filename.endswith('.pt'):
-        model_path = os.path.join(model_folder, filename)
-        try:
-            print(f"\n==== Model: {filename} ====")
-            model = YOLO(model_path)
-            model.info(verbose=True)  # Tampilkan ringkasan detail layer dan jumlah parameter
-        except Exception as e:
-            print(f"Error loading {filename}: {e}")
+# Print the model summary
+model.info()
